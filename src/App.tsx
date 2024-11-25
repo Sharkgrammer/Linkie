82043394eca95b4d4d77b05aab99d9628642db8c
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-11-25 22:29:08 +0000

    Workings

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import links from "./assets/data/links.json";
+import data from "./assets/data/links.json";
 import {LinkItem} from "./types/LinkItem.ts";
+import {Section} from "./types/Section.ts";
 
 function App() {
 
     return (
-        <div>
+        <div className="px-2">
 
-            {links.map((link: LinkItem, index: number) =>
+            {data.map((section: Section, index: number) =>
                 (
-                    <LinkPanel link={link} key={index}/>
+                    <SectionPanel data={section} key={index}/>
                 )
             )}
 
@@ -16,13 +17,35 @@ function App() {
     )
 }
 
+function SectionPanel({data}: { data: Section }) {
+
+    return (
+        <div className="py-2">
+
+            <h1 className="py-1">{data.name}</h1>
+
+            <div className="grid grid-cols-2 gap-2 w-full">
+                {data.links.map((link: LinkItem, index: number) =>
+                    (
+                        <LinkPanel link={link} key={index}/>
+                    )
+                )}
+            </div>
+
+        </div>
+    )
+
+}
+
 function LinkPanel({link}: { link: LinkItem }) {
 
     return (
-        <>
-            <h1>{link.name}</h1>
-            <h1>{link.link}</h1>
-        </>
+        <div className="h-full w-full bg-red-100 border border-green-500 rounded-2xl max-w-32">
+            <a href={link.link}>
+                <p className="w-full text-center p-4 truncate">{link.name}</p>
+            </a>
+        </div>
+
     )
 
 }
---
+++ src/types/Section.ts
@@ -0,0 +1,6 @@
+import {LinkItem} from "./LinkItem.ts";
+
+export interface Section {
+    name: string,
+    links: LinkItem[]
+}
